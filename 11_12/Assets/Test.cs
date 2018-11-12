f9b630ac4f4c7dac4fa47c04fc251f0ff0eecacd
【山本啓太】 <hphp0912@yahoo.co.jp>
2018-11-12 21:36:26 +0900

    変数2

---
+++ 11_12/Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int age;
+        age = 20;
+        Debug.Log(age);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
