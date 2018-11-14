f8eb58ae7a06d520283b4281143723de2d986a44
【山本啓太】 <hphp0912@yahoo.co.jp>
2018-11-14 21:26:41 +0900

    メンバー変数途中

---
+++ 11_14/Assets/Test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    class Name
+    {
+        //メンバー変数
+        private string x;
+        private string y;
+
+
+        public string Myouji()
+        {
+            return x;
+        }
+        public string Namae()
+        {
+            return y;
+        }
+        // Use this for initialization
+        void Start(string x, string y)
+        {
+            Name sougou = new Name();
+            sougou.x = "Yamamoto";
+            sougou.y = "Keita";
+            Debug.Log(x);
+            Debug.Log(y);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
