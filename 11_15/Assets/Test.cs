a9e7bf542ed74a65fa66f03099dfcdd0d819ef3e
【山本啓太】 <hphp0912@yahoo.co.jp>
2018-11-15 21:22:22 +0900

    メンバー変数と条件式

---
+++ 11_15/Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{ 
+ 
+    //メンバー変数を定義
+    private int su_;
+
+	// Use this for initialization
+	void Start () {
+
+
+        su_ = (100);
+
+        if (su_ > 1000)
+        {
+            Debug.Log(su_);
+        }
+            Debug.Log(su_ + 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TestKato.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestKato : MonoBehaviour
+{
+    // メンバー変数を定義.
+    private string str_ = "";
+
+    // Use this for initialization
+    void Start()
+    {
+        str_ = "メンバー変数に文字を代入してログに出力するテスト";
+        Debug.Log(str_);
+    }
+
+}
