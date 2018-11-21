6cc964d7b090f4819d14d0a0ec3b3e65777e3eb4
【山本啓太】 <hphp0912@yahoo.co.jp>
2018-11-21 23:33:57 +0900

    合計値

---
+++ 11_21/Assets/Test2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] points = { 65, 72, 80, 84, 100, 104, 88, 95, -1000, 0 };
+       
+        int sum = 0;
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            if(points[i] > 80 && points[i] < 100)
+            {
+                    sum += points[i];
+               
+            }
+        }
+
+        int average = sum / points.Length;
+        Debug.Log(string.Format("average = {0}", average));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
