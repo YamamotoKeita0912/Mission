d40095147c37d8b89cd8d7804485691947a5cca4
【山本啓太】 <hphp0912@yahoo.co.jp>
2018-12-04 20:16:16 +0900

    VectorClass

---
+++ 11_27_2/Assets/Baseball1.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Batter
+{
+    public int Bpower()
+    {
+        System.Random r = new System.Random(1000);
+        int power = r.Next(10);
+
+        return power;
+    }
+}
+
+public class Pitcher
+{
+    public enum ThrowType
+    {
+        straight,
+        curve,
+        fork,
+        palm,
+        slider
+    }
+
+    public int Ppower()
+    {
+        System.Random r = new System.Random();
+        int power2 = r.Next(10);
+
+        return power2;
+    }
+    
+    
+    public ThrowType Throw()
+    {
+        System.Random r = new System.Random();
+        ThrowType type = (ThrowType)r.Next(4);
+
+        return type;
+    }
+}
+
+public class Baseball1 : MonoBehaviour {
+
+    enum result
+    {
+        StruckOut,
+        Homerun,
+        Hit,
+        TwoBase,
+        ThreeBase
+    }
+
+	// Use this for initialization
+	void Start () {
+        Batter batter = new Batter();
+        Pitcher pitcher = new Pitcher();
+        int bpower = batter.Bpower();
+        int ppower = pitcher.Ppower();
+        
+
+		if(bpower < ppower)
+        {
+            
+            //Debug.Log("三振");
+        }
+        else
+        {
+            
+            //Debug.Log("ホームラン");
+        }
+
+        System.Random r = new System.Random();
+        int ran = r.Next(4);
+        result res = (result)ran;
+        Debug.Log(ran);
+        Debug.Log(res);
+
+        Debug.Log("結果は・・・");
+
+        switch (res)
+        {
+            case result.StruckOut:
+                Debug.Log("三振");
+                break;
+            case result.Homerun:
+                Debug.Log("ホームラン");
+                break;
+            case result.Hit:
+                Debug.Log("ヒット");
+                break;
+            case result.TwoBase:
+                Debug.Log("ツーベース");
+                break;
+            case result.ThreeBase:
+                Debug.Log("スリーベース");
+                break;
+            default:
+                Debug.Log("アウト");
+                break;
+        }
+        
+        Pitcher.ThrowType type  = pitcher.Throw();
+        switch (type)
+        {
+            case Pitcher.ThrowType.curve:
+                Debug.Log("カーブ");
+                break;
+            case Pitcher.ThrowType.fork:
+                Debug.Log("フォーク");
+                break;
+            case Pitcher.ThrowType.palm:
+                Debug.Log("パーム");
+                break;
+            case Pitcher.ThrowType.slider:
+                Debug.Log("スライダー");
+                break;
+            case Pitcher.ThrowType.straight:
+                Debug.Log("ストレート");
+                break;
+            default:
+                Debug.Log("ボーク");
+                break;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 12_4/Assets/Vector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //インスタンスを作成初期化
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        //減算
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+        //magnitudeメンバ変数を使いベクトルdirの長さを求める
+        float len = dir.magnitude;
+        Debug.Log(len);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
